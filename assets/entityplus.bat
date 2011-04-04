349e6377abe3180b821d7508c49052605a8a68d9
Jerry Laman <eraesr@gmail.com>
2011-04-04 10:28:57 +0000

    entityplus startup batch file now loads entityplus map in single player mode.

--- assets/entityplus.bat
+++ assets/entityplus.bat
@@ -1,1 +1,1 @@
-quake3.exe +set fs_game entityplus +set sv_pure 0 +set fraglimit 0 +set timelimit 0 +set sv_maxclients 8 +devmap entityplus
+quake3.exe +set fs_game entityplus +set sv_pure 0 +set fraglimit 0 +set timelimit 0 +set sv_maxclients 8 +spmap entityplus
