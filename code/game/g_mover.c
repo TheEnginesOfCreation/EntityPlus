e518a47cd0deeb6ff9fbc4c1e6eaf6ad77f711dc
Jerry Laman <eraesr@gmail.com>
2011-04-14 19:22:12 +0000

    Setting the wait key on func_door to -2 now prevents the door from closing after it's opened, until it is triggered again.

--- code/game/g_mover.c
+++ code/game/g_mover.c
@@ -580,7 +580,9 @@ void Reached_BinaryMover( gentity_t *ent ) {
 
 		// return to pos1 after a delay
 		ent->think = ReturnToPos1;
-		ent->nextthink = level.time + ent->wait;
+		G_Printf("%f\n", ent->wait);
+		if ( ent->wait != -2000)
+			ent->nextthink = level.time + ent->wait;
 
 		// fire targets
 		if ( !ent->activator ) {
