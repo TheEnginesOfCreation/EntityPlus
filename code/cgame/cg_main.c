58f674af13b295f7688a4cb3585424c8e7cd9ae0
Jerry Laman <eraesr@gmail.com>
2011-06-21 08:41:58 +0000

    - Added scoreboardmusic key to worldspawn entity. - ep_example map now plays music during scoreboard. - updated manual and version history to reflect these changes.

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -1261,6 +1261,18 @@ void CG_StartMusic( void ) {
 
 	trap_S_StartBackgroundTrack( parm1, parm2 );
 }
+
+void CG_StartScoreboardMusic( void ) {
+	char	*s;
+	char	parm1[MAX_QPATH], parm2[MAX_QPATH];
+
+	// start the background music
+	s = (char *)CG_ConfigString( CS_SCOREBOARDMUSIC );
+	Q_strncpyz( parm1, COM_Parse( &s ), sizeof( parm1 ) );
+	Q_strncpyz( parm2, COM_Parse( &s ), sizeof( parm2 ) );
+
+	trap_S_StartBackgroundTrack( parm1, parm2 );
+}
 #ifdef MISSIONPACK
 char *CG_GetMenuBuffer(const char *filename) {
 	int	len;
