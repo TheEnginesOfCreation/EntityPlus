4fa5abe4f5b67da7ee5ec8118700449300fe6b07
eraesr@gmail.com <eraesr@gmail.com@3db1106e-3bc2-ec0d-a47d-0545f0a377c6>
2011-07-13 07:40:52 +0000

    Created tag 0.8.1.

---
+++ tags/0.8.1/code/game/be_ai_gen.h
@@ -0,0 +1,13 @@
+// Copyright (C) 1999-2000 Id Software, Inc.
+//
+
+/*****************************************************************************
+ * name:		be_ai_gen.h
+ *
+ * desc:		genetic selection
+ *
+ * $Archive: /source/code/botlib/be_ai_gen.h $
+ *
+ *****************************************************************************/
+
+int GeneticParentsAndChildSelection(int numranks, float *ranks, int *parent1, int *parent2, int *child);
