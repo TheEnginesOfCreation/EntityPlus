201b9fdd08e4a96c6ac8d05f56720c8df37ef262
Jerry Laman <eraesr@gmail.com>
2011-08-30 20:11:08 +0000

    During cutscenes, player's origin is updated serverside as well now so that the player is always in the same VIS area as the camera.

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -182,6 +182,8 @@ struct gentity_s {
 	char		*key;	// key for target_modify to change
 	char		*value; // value for target_modify to change to
 	int			armor; // armor for the target_playerstats entity
+	vec3_t		orgOrigin; // origin of entity (player) when cutscene starts
+	vec3_t		orgAngles; // view angles entity (player) when cutscene starts
 };
 
 
