b0c3df5b9c6cef07dcea856d66a2c74916a23210
gooball60@gmail.com <gooball60@gmail.com@3db1106e-3bc2-ec0d-a47d-0545f0a377c6>
2011-08-30 02:52:59 +0000

    cleaned up ui_credits a bit

--- code/q3_ui/ui_credits.c
+++ code/q3_ui/ui_credits.c
@@ -4,12 +4,11 @@
 // CONSTANT DEFINITIONS
 
 #define SCROLLSPEED	3.00 // The scrolling speed in pixels per second.
-						  // modify as appropriate for our credits
+						  
 
-// #define BACKGROUND_SHADER
-// uncomment this to use a background shader, otherwise a solid color
-// defined in the vec4_t "color_background" is filled to the screen
-
+//#define BACKGROUND_SHADER
+//uncomment this if you want a background
+ 
 // STRUCTURES
 
 typedef struct {
@@ -18,17 +17,15 @@ typedef struct {
 
 static creditsmenu_t	s_credits;
 
-int starttime; // game time at which credits are started
-float mvolume; // records the original music volume level, as we will
-               // modify it for the credits
+int starttime; 
+float mvolume; 
+               
 
 // change this to change the background colour on credits
-vec4_t color_background	        = {0.00, 0.35, 0.69, 1.00};
-// these are just example colours that are used in credits[] 
-vec4_t color_headertext			= {0.53, 0.77, 1.00, 1.00};
-vec4_t color_maintext			= {1.00, 1.00, 1.00, 1.00};
+vec4_t color_background	        = {0.00, 0.00, 0.00, 1.00};
 
-qhandle_t	BackgroundShader; // definition of the background shader pointer
+
+qhandle_t	BackgroundShader; 
 
 typedef struct
 {
@@ -102,9 +99,6 @@ static sfxHandle_t UI_CreditMenu_Key( int key ) {
 		return 0;
 	}
 
-	// pressing the escape key or clicking the mouse will exit
-	// we also reset the music volume to the user's original
-	// choice here,  by setting s_musicvolume to the stored var
 	trap_Cmd_ExecuteText( EXEC_APPEND, 
                          va("s_musicvolume %f; quit\n", mvolume));
 	return 0;
@@ -122,74 +116,74 @@ static void ScrollingCredits_Draw(void)
   int x = 320, y, n, ysize = 0, fadetime = 0;
   vec4_t fadecolour = { 0.00, 0.00, 0.00, 0.00 };
 
-  // ysize is used to determine the entire length 
-  // of the credits in pixels. 
-  // We can then use this in further calculations
-  if(!ysize) // ysize not calculated, so calculate it dammit!
+  
+  
+  
+  if(!ysize) 
   {
-    // loop through entire credits array
+    
     for(n = 0; n <= sizeof(credits) - 1; n++) 
     {
-      // it is a small character
+      
       if(credits[n].style & UI_SMALLFONT) 
       {
-        // add small character height
+        
         ysize += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
         
-      // it is a big character
+      
       }else if(credits[n].style & UI_BIGFONT) 
       {
-        // add big character size
+        
         ysize += PROP_HEIGHT;
         
-      // it is a huge character
+      
       }else if(credits[n].style & UI_GIANTFONT) 
       {
-        // add giant character size.
+       
         ysize += PROP_HEIGHT * (1 / PROP_SMALL_SIZE_SCALE); 
       }
     }
   }
 
-  // first, fill the background with the specified colour/shader
-  // we are drawing a shader
+  
+  
 #ifdef BACKGROUND_SHADER
   UI_DrawHandlePic(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, BackgroundShader);
 
 #else
-  // we are just filling a color
+  
   UI_FillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, color_background);
 #endif
 
 
-  // let's draw the stuff
-  // set initial y location
+  
+  
   y = 480 - SCROLLSPEED * (float)(uis.realtime - starttime) / 100;
   
-  // loop through the entire credits sequence
+  
   for(n = 0; n <= sizeof(credits) - 1; n++)
   {
-    // this NULL string marks the end of the credits struct
+    
     if(credits[n].string == NULL) 
     {
-      if(y < -16) // credits sequence is completely off screen
+      if(y < -16) 
       {
         trap_Cmd_ExecuteText( EXEC_APPEND, 
                          va("s_musicvolume %f; quit\n", mvolume));
-        break; // end of credits
+        break; 
       }
       break;
     }
 		
-    if( strlen(credits[n].string) == 1) // spacer string, no need to draw
+    if( strlen(credits[n].string) == 1) 
       continue;
 
     if( y > -(PROP_HEIGHT * (1 / PROP_SMALL_SIZE_SCALE))) 
-      // the line is within the visible range of the screen
+      
       UI_DrawProportionalString(x, y, credits[n].string, 
                                 credits[n].style, *credits[n].colour );
 		
-    // re-adjust y for next line
+    
     if(credits[n].style & UI_SMALLFONT)
     {
       y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
@@ -201,7 +195,7 @@ static void ScrollingCredits_Draw(void)
       y += PROP_HEIGHT * (1 / PROP_SMALL_SIZE_SCALE);
     }
 
-	// if y is off the screen, break out of loop
+	
     if (y > 480)
     break;
   }
@@ -220,7 +214,7 @@ void UI_CreditMenu( void ) {
 	s_credits.menu.fullscreen = qtrue;
 	UI_PushMenu ( &s_credits.menu );
 
-	starttime = uis.realtime; // record start time for credits to scroll properly
+	starttime = uis.realtime; 
 	mvolume = trap_Cvar_VariableValue( "s_musicvolume" );
 	if(mvolume < 0.5)
 		trap_Cmd_ExecuteText( EXEC_APPEND, "s_musicvolume 0.5\n" );
@@ -229,6 +223,6 @@ void UI_CreditMenu( void ) {
 	// load the background shader
 #ifdef BACKGROUND_SHADER
 	BackgroundShader =
-		trap_R_RegisterShaderNoMip("**");
+		trap_R_RegisterShaderNoMip("**"); //uncomment line 9 if you want a background
 #endif
 }
