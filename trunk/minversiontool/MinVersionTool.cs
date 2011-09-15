c26183ca8ec53ddfd57188efc915f57d1854c1db
eraesr@gmail.com <eraesr@gmail.com@3db1106e-3bc2-ec0d-a47d-0545f0a377c6>
2011-09-15 14:36:00 +0000

    WiP minversiontool

--- trunk/minversiontool/MinVersionTool.cs
+++ trunk/minversiontool/MinVersionTool.cs
@@ -14,7 +14,11 @@ namespace mvt
 		#endregion
 
 		
-		#region enums
+		#region definitions
+		/// <summary>The default minversion. Should always be 1.0</summary>
+		private const Versions DEFAULT_MINVERSION = Versions.one_zero;
+
+		/// <summary>Statuses for the map file parser</summary>
 		private enum ParserStatus
 		{
 			Normal = 0,
@@ -24,6 +28,62 @@ namespace mvt
 			ReadingBrush = 4,
 			ReadingPatch = 5
 		}
+
+		/// <summary>List of supported EntityPlus versions. Numeric enum values match the indexes in the VersionsStrings array</summary>
+		private enum Versions
+		{
+			UnableToDetect = 0,
+			one_zero = 1
+		}
+
+		/// <summary>List of strings for supported EntityPlus versions</summary>
+		private string[] VersionsStrings =
+		{
+			"Unable to determine due to unknown entity classnames.",
+			"1.0"
+		};
+
+		/// <summary>A list of all the known entity classnames</summary>
+		private string[] KnownEntities = 
+		{ 
+			//worldspawn and q3map2 entities
+			"_decal", "_skybox", "worldspawn", 
+			//ammo_
+			"ammo_bfg", "ammo_bullets", "ammo_cells", "ammo_grenades", "ammo_lightning", "ammo_rockets", "ammo_shells", "ammo_slugs", 
+			//func_
+			"func_bobbing", "func_breakable", "func_button", "func_door", "func_door_rotating", "func_group", "func_pendulum", "func_plat", 
+			"func_rotating", "func_static", "func_timer", "func_train",
+			//holdable_
+			"holdable_key_blue", "holdable_key_gold", "holdable_key_green", "holdable_key_iron", "holdable_key_master", "holdable_medkit", 
+			"holdable_key_red", "holdable_key_silver", "holdable_teleporter", "holdable_key_yellow",
+			//info_
+			"info_backpack", "info_camera", "info_camp", "info_notnull", "info_null", "info_player_intermission", "info_player_start",
+			"info_waypoint", "info_firstplace", "info_secondplace", "info_thirdplace", "info_player_deathmatch",
+			//item_
+			"item_armor_body", "item_armor_combat", "item_armor_vest", "item_armor_shard", "item_botroam", "item_enviro", "item_flight",
+			"item_haste", "item_health", "item_health_large", "item_health_mega", "item_health_small", "item_invis", "item_quad",
+			"item_regen",
+			//light
+			"light",
+			//misc_
+			"misc_model", "misc_portal_camera", "misc_portal_surface", "misc_teleporter_dest", 
+			//path_
+			"path_corner",
+			//shooter_
+			"shooter_bfg", "shooter_grenade", "shooter_plasma", "shooter_rocket",
+			//target_
+			"target_botspawn", "target_cutscene", "target_debrisemitter", "target_delay", "target_earthquake", "target_effect",
+			"target_finish", "target_give", "target_gravity", "target_kill", "target_laser", "target_logic", "target_mapchange",
+			"target_modify", "target_objective", "target_playerspeed", "target_playerstats", "target_position", "target_print",
+			"target_push", "target_relay", "target_remove_powerups", "target_score", "target_script", "target_secret", "target_skill",
+			"target_speaker", "target_teleporter", "target_unlink", "target_variable",
+			//trigger_
+			"trigger_always", "trigger_death", "trigger_frag", "trigger_lock", "trigger_hurt", "trigger_multiple", "trigger_push",
+			"trigger_teleport",
+			//weapon_
+			"weapon_bfg", "weapon_gauntlet", "weapon_grapplinghook", "weapon_grenadelauncher", "weapon_lightning", "weapon_machinegun",
+			"weapon_plasmagun", "weapon_railgun", "weapon_rocketlauncher", "weapon_shotgun", 
+		};
 		#endregion
 
 
@@ -31,20 +91,22 @@ namespace mvt
 		public MinVersionTool(string filename, bool debug)
 		{
 			this.debug = debug;
-			ParseMap(filename);
+			Versions minversion = ParseMap(filename);
 
-			Console.WriteLine("===============");
 			Console.WriteLine("Finished parsing \"" + filename + "\"");
-			Console.WriteLine("\nminversion = 1.0");
-			Console.WriteLine("===============");
+			Console.WriteLine("\nminversion = " + VersionsStrings[(int)minversion]);
 			Console.ReadKey(true);
 		}
 		#endregion
 
 
 		#region private methods
-		private void ParseMap(string filename)
+		/// <summary>Parses a complete .map file</summary>
+		/// <param name="filename">Filename of the map to parse</param>
+		/// <returns>The minimum version of EntityPlus that's required to run this map</returns>
+		private Versions ParseMap(string filename)
 		{
+			Versions minversion = DEFAULT_MINVERSION;
 			StreamReader sr = File.OpenText(filename);
 
 			ParserStatus parserStatus = ParserStatus.Normal;
@@ -79,6 +141,7 @@ namespace mvt
 				{
 					parserStatus = ParserStatus.Normal;
 					Debug(String.Format("Entity {0}: {1}", ent.EntityNum, ent.GetValue("classname")));
+					minversion = CheckEntity(ent, minversion);
 					entities.Add(ent);
 				}
 				else if (line == "}" && parserStatus == ParserStatus.ReadingBrush)
@@ -92,8 +155,12 @@ namespace mvt
 			}
 
 			sr.Close();
+			return minversion;
 		}
 
+		/// <summary>Parses a single key/value combination for an entity</summary>
+		/// <param name="line">The line containing the key/value information</param>
+		/// <param name="ent">The entity object to which the key/value pair should be added</param>
 		private void ParseKeyValue(string line, Entity ent)
 		{
 			int startKey = line.IndexOf("\"") + 1;
@@ -107,6 +174,40 @@ namespace mvt
 			ent.AddKeyValuePair(key, value);
 		}
 
+		/// <summary>Checks the version of EntityPlus that's required to support the entity in this form.</summary>
+		/// <param name="ent">The entity to check.</param>
+		/// <param name="currentVersion">The currently determined minversion for the map as far as it's parsed.</param>
+		/// <returns>The minimum EntityPlus version required to support this entity</returns>
+		private Versions CheckEntity(Entity ent, Versions currentVersion)
+		{
+			if (!IsKnownEntity(ent.GetValue("classname")))
+			{
+				Console.WriteLine("WARNING: Unknown classname \"" + ent.GetValue("classname") + "\"");
+				return Versions.UnableToDetect;
+			}
+
+			return currentVersion;
+		}
+
+		/// <summary>
+		/// Checks if MinVersionTool is aware of this entity type. It could be that someone uses an older MinVersionTool that's not aware of
+		/// newer entities. MinVersionTool reports any unknown classnames because such classnames prevent it from accurately detecting the
+		/// minimum version required to run this map.
+		/// </summary>
+		/// <param name="classname">The classname to check</param>
+		/// <returns>True if MinVersionTool recognizes the classname, false if not.</returns>
+		private bool IsKnownEntity(string classname)
+		{
+			foreach (string cn in KnownEntities)
+			{
+				if (cn == classname)
+					return true;
+			}
+			return false;
+		}
+
+		/// <summary>Prints a debug message (only if the -d commandline switch was supplied)</summary>
+		/// <param name="text">Debug text to print</param>
 		private void Debug(string text)
 		{
 			if (debug)
