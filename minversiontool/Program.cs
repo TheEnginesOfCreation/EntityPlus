f132e49afae3a75c3acd0c5a3eccff07d7997c01
Jerry Laman <eraesr@gmail.com>
2011-09-15 13:02:04 +0000

    Added first code set up for minversiontool

---
+++ minversiontool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace mvt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filename= "";
+			bool debug = false;
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				if (args[i] == "-d")
+					debug = true;
+				else if (filename == "")
+					filename = args[i];
+			}
+
+			if (filename == "")
+			{
+				Console.WriteLine("You must supply the path to a .map file");
+				Console.ReadKey();
+				return;
+			}
+
+			if (!File.Exists(filename))
+			{
+				Console.WriteLine("The specified map file could not be found");
+				Console.ReadKey();
+				return;
+			}
+
+			MinVersionTool mvt = new MinVersionTool(filename, debug);
+		}
+	}
+}
