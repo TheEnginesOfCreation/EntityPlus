1b7c522d81bc8567d78fd8f728ba2f4b6e1743da
eraesr@gmail.com <eraesr@gmail.com@3db1106e-3bc2-ec0d-a47d-0545f0a377c6>
2011-10-06 09:29:35 +0000

    - MinVersionTool reports which version of EntityPlus it supports - Added detection of target_botremove entity to MinVersionTool - MinVersionTool no longer crashes when a key has been specified twice. It will now show an error instead.

--- trunk/minversiontool/Entity.cs
+++ trunk/minversiontool/Entity.cs
@@ -16,6 +16,11 @@ namespace mvt
 
 		public void AddKeyValuePair(string key, string value)
 		{
+			if (KeyValuePairs.ContainsKey(key))
+			{
+				Console.WriteLine(String.Format("Error: entity {0} contains multiple entries for \"{1}\" key", EntityNum, key));
+				return;
+			}
 			KeyValuePairs.Add(key, value);
 		}
 
