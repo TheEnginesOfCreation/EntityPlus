b7c6449de69e6f743f413fe91c2384ac37f955ab
eraesr@gmail.com <eraesr@gmail.com@3db1106e-3bc2-ec0d-a47d-0545f0a377c6>
2011-10-25 09:01:33 +0000

    cg_drawTimer is again allowed when cheats are enabled and the timer displays milliseconds as well.

--- trunk/code/cgame/cg_main.c
+++ trunk/code/cgame/cg_main.c
@@ -203,7 +203,7 @@ static cvarTable_t cvarTable[] = { // bk001129
 	{ &cg_draw2D, "cg_draw2D", "1", CVAR_ARCHIVE  },
 	{ &cg_drawStatus, "cg_drawStatus", "1", CVAR_ARCHIVE  },
 	{ &cg_drawHoldableItems, "cg_drawHoldableItems", "1", CVAR_ARCHIVE  },
-	{ &cg_drawTimer, "cg_drawTimer", "0", CVAR_ARCHIVE  },
+	{ &cg_drawTimer, "cg_drawTimer", "0", CVAR_ARCHIVE | CVAR_CHEAT },
 	{ &cg_drawFPS, "cg_drawFPS", "0", CVAR_ARCHIVE  },
 	{ &cg_drawSnapshot, "cg_drawSnapshot", "0", CVAR_ARCHIVE  },
 	{ &cg_draw3dIcons, "cg_draw3dIcons", "1", CVAR_ARCHIVE  },
