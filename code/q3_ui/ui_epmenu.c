50dcf1d72a6be805dc5c123fb24b276839c2eb3e
Jerry Laman <eraesr@gmail.com>
2011-12-20 10:24:15 +0000

    - Changed epversion to 1.1.1 - Added minversion support for 1.1.1 - Updated documentation

--- code/q3_ui/ui_epmenu.c
+++ code/q3_ui/ui_epmenu.c
@@ -408,7 +408,8 @@ qboolean EPMenu_VersionAccepted( char *version ) {
 			Q_stricmp( version, "1.0" ) != 0 && 
 			Q_stricmp( version, "1.0.1" ) != 0 &&
 			Q_stricmp( version, "1.0.2" ) != 0 &&
-			Q_stricmp( version, "1.1" ) != 0
+			Q_stricmp( version, "1.1" ) != 0 &&
+			Q_stricmp( version, "1.1.1" ) != 0
 		)
 		{
 			return qfalse;
