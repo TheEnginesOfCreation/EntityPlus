72fa36a722ced19726f9557aa344f1668005737b
eraesr@gmail.com <eraesr@gmail.com@3db1106e-3bc2-ec0d-a47d-0545f0a377c6>
2011-12-30 11:03:58 +0000

    Created tag 1.1.2.

---
+++ tags/1.1.2/code/cgame/cg_info.c
@@ -0,0 +1,135 @@
+// Copyright (C) 1999-2000 Id Software, Inc.
+//
+// cg_info.c -- display information while data is being loading
+
+#include "cg_local.h"
+
+#define MAX_LOADING_PLAYER_ICONS	16
+#define MAX_LOADING_ITEM_ICONS		26
+
+static int			loadingPlayerIconCount;
+static int			loadingItemIconCount;
+static qhandle_t	loadingPlayerIcons[MAX_LOADING_PLAYER_ICONS];
+static qhandle_t	loadingItemIcons[MAX_LOADING_ITEM_ICONS];
+
+
+/*
+===================
+CG_DrawLoadingIcons
+===================
+*/
+static void CG_DrawLoadingIcons( void ) {
+	int		n;
+	int		x, y;
+
+	for( n = 0; n < loadingPlayerIconCount; n++ ) {
+		x = 16 + n * 78;
+		y = 324-40;
+		CG_DrawPic( x, y, 64, 64, loadingPlayerIcons[n] );
+	}
+
+	for( n = 0; n < loadingItemIconCount; n++ ) {
+		y = 400-40;
+		if( n >= 13 ) {
+			y += 40;
+		}
+		x = 16 + n % 13 * 48;
+		CG_DrawPic( x, y, 32, 32, loadingItemIcons[n] );
+	}
+}
+
+
+/*
+======================
+CG_LoadingString
+
+======================
+*/
+void CG_LoadingString( const char *s ) {
+	Q_strncpyz( cg.infoScreenText, s, sizeof( cg.infoScreenText ) );
+
+	trap_UpdateScreen();
+}
+
+/*
+===================
+CG_LoadingItem
+===================
+*/
+void CG_LoadingItem( int itemNum ) {
+	gitem_t		*item;
+
+	item = &bg_itemlist[itemNum];
+	
+	if ( item->icon && loadingItemIconCount < MAX_LOADING_ITEM_ICONS ) {
+		loadingItemIcons[loadingItemIconCount++] = trap_R_RegisterShaderNoMip( item->icon );
+	}
+
+	CG_LoadingString( item->pickup_name );
+}
+
+/*
+===================
+CG_LoadingClient
+===================
+*/
+void CG_LoadingClient( int clientNum ) {
+	const char		*info;
+	char			*skin;
+	char			personality[MAX_QPATH];
+	char			model[MAX_QPATH];
+	char			iconName[MAX_QPATH];
+
+	info = CG_ConfigString( CS_PLAYERS + clientNum );
+
+	if ( loadingPlayerIconCount < MAX_LOADING_PLAYER_ICONS ) {
+		Q_strncpyz( model, Info_ValueForKey( info, "model" ), sizeof( model ) );
+		skin = Q_strrchr( model, '/' );
+		if ( skin ) {
+			*skin++ = '\0';
+		} else {
+			skin = "default";
+		}
+
+		Com_sprintf( iconName, MAX_QPATH, "models/players/%s/icon_%s.tga", model, skin );
+		
+		loadingPlayerIcons[loadingPlayerIconCount] = trap_R_RegisterShaderNoMip( iconName );
+		if ( !loadingPlayerIcons[loadingPlayerIconCount] ) {
+			Com_sprintf( iconName, MAX_QPATH, "models/players/characters/%s/icon_%s.tga", model, skin );
+			loadingPlayerIcons[loadingPlayerIconCount] = trap_R_RegisterShaderNoMip( iconName );
+		}
+		if ( !loadingPlayerIcons[loadingPlayerIconCount] ) {
+			Com_sprintf( iconName, MAX_QPATH, "models/players/%s/icon_%s.tga", DEFAULT_MODEL, "default" );
+			loadingPlayerIcons[loadingPlayerIconCount] = trap_R_RegisterShaderNoMip( iconName );
+		}
+		if ( loadingPlayerIcons[loadingPlayerIconCount] ) {
+			loadingPlayerIconCount++;
+		}
+	}
+
+	Q_strncpyz( personality, Info_ValueForKey( info, "n" ), sizeof(personality) );
+	Q_CleanStr( personality );
+
+	CG_LoadingString( personality );
+}
+
+/*
+====================
+CG_DrawInformation
+
+Draw all the status / pacifier stuff during level loading
+====================
+*/
+void CG_DrawInformation( void ) {
+	const char	*s;
+	const char	*info;
+	const char	*sysInfo;
+	int			y;
+	int			value;
+	qhandle_t	levelshot;
+	qhandle_t	detail;
+	char		buf[1024];
+
+	return;
+}
+
---
+++ tags/1.1.2/minversiontool/Entity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvt
+{
+	class Entity
+	{
+		#region Private fields
+		private Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>();
+		#endregion
+
+		
+		#region Properties
+		public int EntityNum
+		{
+			get;
+			set;
+		}
+
+		public int Spawnflags
+		{
+			get
+			{
+				string sf = GetValue("spawnflags");
+				if (String.IsNullOrEmpty(sf))
+					return 0;
+				else
+				{
+					try
+					{
+						int sfi = int.Parse(sf);
+						return sfi;
+					}
+					catch (FormatException)
+					{
+						Console.WriteLine("ERROR: spawnflags for this entity is set to \"" + sf + "\", which is not a valid numeric value");
+						return 0;
+					}
+				}
+			}
+		}
+
+		public string Classname
+		{
+			get
+			{
+				return GetValue("classname");
+			}
+		}
+		#endregion
+
+
+		public Entity()
+		{
+		}
+
+		public void AddKeyValuePair(string key, string value)
+		{
+			key = key.ToLower();
+			if (KeyValuePairs.ContainsKey(key))
+			{
+				Console.WriteLine(String.Format("Error: entity {0} contains multiple entries for \"{1}\" key", EntityNum, key));
+				return;
+			}
+			KeyValuePairs.Add(key, value);
+		}
+
+		public string GetValue(string key)
+		{
+			key = key.ToLower();
+			if (KeyValuePairs.ContainsKey(key))
+				return KeyValuePairs[key];
+			else
+				return null;
+		}
+	}
+}
