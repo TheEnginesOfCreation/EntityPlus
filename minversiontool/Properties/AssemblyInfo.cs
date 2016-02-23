ccba894a9bf3935ae9056fc1e1af0ca3da2e2105
Eraser <eraesr@gmail.com>
2016-02-23 21:54:25 +0100

    MinVersionTool Assembly info

--- minversiontool/Properties/AssemblyInfo.cs
+++ minversiontool/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mvt")]
+[assembly: AssemblyTitle("MinVersion Tool application")]
 [assembly: AssemblyDescription("MinVersion Tool for EntityPlus")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mvt")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCompany("The Engines of Creation")]
+[assembly: AssemblyProduct("MinVersion Tool for EntityPlus")]
+[assembly: AssemblyCopyright("Copyright © 2016 The Engines of Creation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyVersion("1.1.7.0")]
+[assembly: AssemblyFileVersion("1.1.7.0")]
