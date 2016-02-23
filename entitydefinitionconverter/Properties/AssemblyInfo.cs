504eb02b2ac2a244624669660fb4c0b7f114622d
Eraser <eraesr@gmail.com>
2016-02-23 22:04:16 +0100

    Updated EDC assembly info

--- entitydefinitionconverter/Properties/AssemblyInfo.cs
+++ entitydefinitionconverter/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("edc")]
+[assembly: AssemblyTitle("EntityDefinitionsConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("edc")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCompany("The Engines of Creation")]
+[assembly: AssemblyProduct("EntityDefinitionsConverter")]
+[assembly: AssemblyCopyright("Copyright ©  2011 The Engines of Creation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
